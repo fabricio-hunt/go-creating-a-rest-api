112017b3390c44b4c42620ea1dc694bde5914a64
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2025-12-03 22:14:31 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+//principal function
+func main() {
+
+	//variable declaration style short	assignment
+	nomePizzaria := "Pizzaria GO"
+	instagram, phone := "pizzariago", "1234-5678"
+
+	//print values
+	println("Nome:", nomePizzaria)
+	println("Informações da Pizzaria:")
+	println("Instagram:", instagram)
+	println("Telefone:", phone)
+
+}
